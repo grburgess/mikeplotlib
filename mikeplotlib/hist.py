f31c74e55fbb7f5022a8df915fcac4cfe6b4de12
grburgess <jmichaelburgess@gmail.com>
2019-11-16 11:21:52 +0100

    hist is working

--- mikeplotlib/hist.py
+++ mikeplotlib/hist.py
@@ -1,20 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from colour import Color
+import matplotlib.colors as colors
 
+from mikeplotlib.utils.colorscale import colorscale
 
-
-def hist(data, color='r', ax=None, **kwargs):
+def hist(data, color='#C61C1C', ax=None, **kwargs):
 
     if ax is None:
 
         ax = plt.gca()
 
+    if 'lw' in kwargs:
+
+        lw = kwargs.pop('lw')
+
+    else:
+
+        lw = 2
+        
     # compute the colors
 
+    color = colors.to_hex(color)
+    
     fc = color
-    ec = None
 
-    artist = ax.hist(data, fc=fc, ec=ec, histtype='step', **kwargs)
+    # now compute the edge color
+
+    c_color = Color(color)
+
+    HL_scale = .85
+    
+    ec = colorscale(c_color.hex, HL_scale)
+
+    artist = ax.hist(data, fc=fc, ec=ec, lw=lw,**kwargs)
 
     return artist
     
